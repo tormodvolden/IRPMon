386393a78286935a07e23c4b2a520449b3faa301
Tormod Volden <debian.tormod@gmail.com>
2024-02-17 15:30:30 +0100

    mingw: Silence (tons of) multi-char warnings

--- common-km.mk
+++ common-km.mk
@@ -1,6 +1,7 @@
 
 CFLAGS += -DNTDDI_VERSION=NTDDI_WIN7
 CFLAGS += -D_KERNEL_MODE
+CFLAGS += -Wno-multichar
 
 INCLUDES +=  -I../km-shared -I../include -I../shared -I$(MINGW_INCLUDE)/ddk -I$(MINGW_INCLUDE)
 PREINCLUDES = -I../mingw-headers
